efb3a4a85c0fc78178cd6cbfb8d5a75fab4ffebc
Daniel Wendon-Blixrud <d@nielwb.com>
2020-07-16 22:06:53 +0100

    resized images

--- docs/main.py
+++ docs/main.py
@@ -92,7 +92,7 @@ def convert(in_folder, out_folder):
         pillow_image = Image.open(in_url)
         trimmed_image = trim_image(pillow_image)
         size = trimmed_image.size
-        scale = 0.4
+        scale = 1
         resize = (int(size[0]*scale), int(size[1]*scale))
         resized_image = trimmed_image.resize( resize )
         resized_image.save(out_url)
